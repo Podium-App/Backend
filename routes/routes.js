bd413652763061b5f00ee3bd544f998fad53ff62
James J. Njong <njongjames@outlook.com>
2023-08-16 01:52:51 +0100

    adding endpoint for filtering results for a single category

--- routes/routes.js
+++ routes/routes.js
@@ -16,14 +16,7 @@ module.exports = (app) =>{
     app.route('/addsubject').post(async (req,res,next)=>{
             const {subjectname, scope, questions} = req.body; 
 
-            const response = await controller.createExam({subjectname, scope, questions}); 
-
-            if(response.status == 400){
-                res.status(400).send({response})
-            }else{
-                res.status(200).send({response})
-            }
-
+            //const response = await controller.createExam({subjectname, scope, questions}); 
     })
 
 
@@ -40,14 +33,33 @@ module.exports = (app) =>{
 
     app.route('/podium/onesubject').get(async (req,res) =>{
         //TODO: authorize response
-        const {subjectname} = req.body; 
-        console.log(subjectname) 
+        const {subjectname} = req.body;  
         const response = await controller.getTest(subjectname)
         if(response.status == 400){
                 res.status(400).send({response})
             }else{
                 res.status(200).send({response})
             }
+    })
+
+    app.route("/podium/onecategory").get(async (req,res)=>{
+        const {scope} = req.body; 
+
+        const response = await controller.getAllSubjects(); 
+
+        
+
+        if(response.status == 400){
+                res.status(400).send({response})
+            }else{
+                const filteredResponse = response.response.filter(
+                    (item) =>{
+                        return item.scope === scope
+                    }
+                )
+                res.status(200).send({"status":200,filteredResponse})
+            }
+
     })
 
 }
