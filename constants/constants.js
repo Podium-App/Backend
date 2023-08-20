a2b2084f8c0b4a36221070be4bc8954a91a3ba13
James J. Njong <njongjames@outlook.com>
2023-08-20 01:42:35 +0100

    updating responses to have standard format so they can easily be processed on android and web frontend

--- constants/constants.js
+++ constants/constants.js
@@ -3,4 +3,24 @@ require('dotenv').config();
 module.exports = {
     SERVER_PORT: process.env.PORT || 6000,
     SERVER_DB_URI: process.env.mongo, 
+    Token_My_Secret: process.env.mySecret,
+
+    Messages: {
+        msg1:"Successfully retrieved list of subjects",
+        msg11:"Failed retrieve list of subjects", 
+        msg2:"Successfully retrieved requested test", 
+        msg21:"Failed to find requested test. No such subject in the database",
+        msg22:"Failed to load session. Number of questions exceed available question list.", 
+        msg3:"Successfully found the subject searched",
+        msg31:"Cannot find  any instance of the requested search.",
+        msg4:"Successfully updated records for this subject",
+        msg41:"Failed to updated record for this subject",
+        msg5:"Successfully retrieved records for the given subject",
+        msg51:"Failed to find records for the given subject",
+        msg6:"Successfully created new user",
+        msg61:"Failed to create new user",
+        msg7:"Successfully identified user",
+        msg71:"Failed to identify this user",
+        msg8:"We are unable to verify your access credentials."
+    }
 }
--- controller/controller.js
+++ controller/controller.js
@@ -3,6 +3,8 @@
 
 const Subjects = require('../database/Subjects'); 
 const Test = require("../database/Test"); 
+const cts = require('../constants/constants'); 
+const responder = require("../controller/responder.js"); 
 
 
 module.exports.createExam = async ({subjectname, scope, questions}) =>{
@@ -29,10 +31,22 @@ module.exports.getAllSubjects = async () =>{
     const response = await Subjects.find(); 
 
     if(!response){
-        return {"status":400}; 
+        const result = {
+            "responseFlag":"Failed", 
+            "responseMessage": "Operation not successful", 
+            "resultData":null
+        }
+        return responder.getResponse({"status":400,"message": cts.Messages.msg11, result }); 
     }else{
-        return{"status":200, response}; 
-    }
+        
+        const subjectList =  formatSubjectReponse(response); 
+        const result = {
+            "responseFlag":"Ok", 
+            "responseMessage": "Operation successful", 
+            "resultData":subjectList
+        }
+        return responder.getResponse({"status":200, "message":cts.Messages.msg1, result}) ; 
+    } 
 }
 
 
@@ -40,8 +54,29 @@ module.exports.getTest = async(subjectname)=>{
     const response = await Test.findOne({subjectname});  
     
     if(!response){
-        return {"status":400, "Error": "Could not find test."}; 
+        const result = {
+            "responseFlag":"Failed", 
+            "responseMessage": "Operation not successful", 
+            "resultData":null
+        }
+        return responder.getResponse({"status":400, "message": cts.Messages.msg31,result}); 
     }else{
-        return{"status":200, response}; 
+        const result = {
+            "responseFlag":"Ok", 
+            "responseMessage": "Operation successful", 
+            "resultData":response
+        }
+        return responder.getResponse({"status":200, "message": cts.Messages.msg3,result}); 
+        
     }
+}
+
+
+function formatSubjectReponse(response){
+    let subjects = []
+    response.forEach(item =>{
+        const {subjectname,scope,counts,_id} = item 
+        subjects.push({subjectname,scope,counts,_id })
+    })
+    return subjects
 }
